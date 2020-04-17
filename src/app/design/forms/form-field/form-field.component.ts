08484f3fc81cfcbdaa10d42a05ddcfb2822889dc
Mirage <mirage.dt.2020@gmail.com>
2020-04-17 23:21:32 +0200

    Added password visibility toggle

--- src/app/design/forms/form-field/form-field.component.ts
+++ src/app/design/forms/form-field/form-field.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'mg-form-field',
   templateUrl: './form-field.component.html',
   styleUrls: ['./form-field.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FormFieldComponent implements OnInit {
+export class FormFieldComponent implements AfterContentInit {
+  isPassword = false;
+  showPassword = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get input() {
+    return this.el.nativeElement.querySelector('input');
   }
 
+  constructor(private el: ElementRef<HTMLElement>) {}
+
+  ngAfterContentInit() {
+    this.isPassword = this.input.type === 'password';
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+    if (this.showPassword) this.input.type = 'text';
+    else this.input.type = 'password';
+  }
 }
--- src/app/design/forms/forms.module.ts
+++ src/app/design/forms/forms.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormFieldComponent } from './form-field/form-field.component';
 import { RadioGroupComponent } from './radio-group/radio-group.component';
 import { RadioButtonComponent } from './radio-group/radio-button/radio-button.component';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @NgModule({
   declarations: [FormFieldComponent, RadioGroupComponent, RadioButtonComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
   ],
   exports: [FormFieldComponent, RadioGroupComponent, RadioButtonComponent],
 })
