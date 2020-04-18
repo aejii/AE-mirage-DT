8c3c31eee13c74a784fbdf26efc9e7f7ab34fb5e
Mirage <mirage.dt.2020@gmail.com>
2020-04-18 22:00:37 +0200

    v1.1.0

--- docker.sh
+++ docker.sh
@@ -1,10 +1,12 @@
 if docker run \
+  --rm \
   -it \
   -v /$(pwd):/usr/src/mirage maryannah/cordova:latest npm run release ; then
     echo "Running TTY in Docker"
 else
     echo "Running WINPTY in Docker"
     winpty docker run \
+      --rm \
       -it \
       -v /$(pwd):/usr/src/mirage maryannah/cordova:latest npm run release
 fi
