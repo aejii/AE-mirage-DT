41d313bc798d98a25c1d287eddd342098a0fcf45
Mirage <mirage.dt.2020@gmail.com>
2020-04-19 14:36:31 +0200

    Added function to find a key in the game

--- src/assets/mock.script.js
+++ src/assets/mock.script.js
@@ -39,3 +39,50 @@ window.cordova = {
     },
   },
 };
+
+function findKey(propToFind, obj = window, maxDepth = 10) {
+  let path = `[YOUR OBJ]`;
+  let refs = [];
+  let results = [];
+
+  const canRecursive = (o, k, d) =>
+    typeof o === 'object' &&
+    !refs.includes(o) &&
+    o !== window &&
+    !['self', '_parent'].includes(k) &&
+    d < maxDepth &&
+    !(o instanceof Element);
+
+  const recursive = (currObj, currPath, depth = 0) => {
+    for (const key in currObj) {
+      const newPath = `${currPath}.${key}`;
+      const newObj = currObj[key];
+      if (key === propToFind) {
+        console.log('Found in', newPath);
+        results.push(newPath);
+      } else {
+        if (typeof newObj === 'object' && canRecursive(newObj, key, depth)) {
+          refs.push(newObj);
+          recursive(newObj, newPath, depth + 1);
+        }
+        if (Array.isArray(newObj)) {
+          newObj.forEach((o) => {
+            if (
+              typeof o === 'object' &&
+              canRecursive(o, key, depth)
+            ) {
+              refs.push(o);
+              recursive(o, newPath, depth + 1);
+            }
+          });
+        }
+      }
+    }
+  };
+
+  console.log('Starting ...');
+  recursive(obj, path);
+  console.log('DONE !');
+
+  return results;
+}
