69665e6b561a3f7fcbbcccf0aa86047e44f9d172
MirageDT <70582541+MirageDT@users.noreply.github.com>
2020-10-04 21:12:44 +0200

    Added Mac OSX build

---
+++ scripts/mac.build.sh
@@ -0,0 +1,28 @@
+# Stop the script on error
+set -o errexit
+
+# Build the application
+npx ng build -c electron-prod
+
+# Create a reference package.json
+touch electron/package.json && echo {} > electron/package.json
+
+# Build for Mac OSX
+npx electron-packager electron Mirage --out electron/dist --platform=darwin --arch=x64 --asar --icon=src/app/assets/logo.ico
+
+mkdir release
+mkdir release/desktop
+
+# For each directory of the dist folder
+for dirpath in `find ./electron/dist/ -maxdepth 1 -mindepth 1 -type d`
+do
+  # Get only the dirname from the dirpath
+  dirname=$(basename "$dirpath")
+  # Zip the folder
+  cd electron/dist && zip -r "$dirname".zip "$dirname" && cd -
+  # Move the folder to dist (copy to avoid having file lock errors)
+  cp -r electron/dist/"$dirname".zip release/desktop
+done
+
+# Remove the artifacts
+rm -rf electron/dist electron/app electron/package.json
